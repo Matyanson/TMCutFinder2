15a434af021dc6ba93d469419bf2ce6dbb64b2c2
Matyanson <matyas.perem@seznam.cz>
2021-08-14 16:24:47 +0200

    feat(): coords helper functions

---
+++ src/utils/functions.ts
@@ -0,0 +1,11 @@
+import type Coords from "src/models/Coords";
+
+export const getDist = (pointA: Coords, pointB: Coords) => {
+    return Math.sqrt(
+        Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointA.y, 2)
+    );
+}
+
+export const addPoints = (pointA: Coords, pointB: Coords) => {
+    return {x: pointA.x + pointB.x, y: pointA.y + pointB.y}
+}
