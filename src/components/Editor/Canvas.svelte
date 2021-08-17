f0f3bfd1d4338a0834b35f57fb68426992af41c4
Matyanson <matyas.perem@seznam.cz>
2021-08-18 01:13:52 +0200

    fix(canvas): polyline points are percentage

--- src/components/Editor/Canvas.svelte
+++ src/components/Editor/Canvas.svelte
@@ -1,8 +1,15 @@
 <div class="canvas" bind:this={svg}>
-    <svg >
+    <svg viewBox="0 0 100 100" preserveAspectRatio="none">
         <marker id="arrow" viewBox="0 0 10 6" refX="10" refY="3" markerWidth="6" markerHeight="6" orient="auto-start-reverse">
             <path d="M 0 0 L 10 3 L 0 6 z" fill="#ddd"/>
         </marker>
+        {#each $paths as path}
+            <polyline class='path' points={pointsToPath(path.points)} vector-effect="non-scaling-stroke"  stroke-width='10px' fill='none' stroke={'#1b36ca'}/>
+        {/each}
+        <polyline class='path' points='0,0 0,100 100,100 100,0' vector-effect="non-scaling-stroke"  stroke-width='10px' fill='none' stroke={'#1b36ca'}/>
+    </svg>
+    <svg>
+        <circle cx={'50%'} cy={'50%'} r={10} fill={'white'} />
         <text x={'0%'} y={'100%'} fill={'white'}>{`${Math.floor(m.x)}:${Math.floor(m.y)}`}</text>
         <circle cx={'10%'} cy={'10%'} r={10} fill={'white'} />
         <circle cx={'50%'} cy={'50%'} r={10} fill={'white'} />
