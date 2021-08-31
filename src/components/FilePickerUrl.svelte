004554f86534234f8068601b402121cbfeca8d17
Matyanson <matyas.perem@seznam.cz>
2021-08-31 21:26:37 +0200

    fix(): event parameter changed to file

--- src/components/FilePickerUrl.svelte
+++ src/components/FilePickerUrl.svelte
@@ -5,14 +5,13 @@ import FilePicker from "./FilePicker.svelte";
     export let text: string = undefined;
     export let onChange: (res: string) => void;
 
-    function saveUrl(event: Event){
-        const target = event.target as HTMLInputElement;
+    function saveUrl(file: File){
         let reader = new FileReader();
         reader.onload = () => {
             if(typeof(reader.result) == "string")
                 onChange(reader.result);
         }
-        reader.readAsDataURL(target.files[0]);
+        reader.readAsDataURL(file);
     }
 </script>
 
