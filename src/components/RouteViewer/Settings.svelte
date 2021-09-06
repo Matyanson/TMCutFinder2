13b1e9e6ee47a2fed3f8c7f091d8914aaaa9a7f6
Matyanson <matyas.perem@seznam.cz>
2021-09-06 19:53:45 +0200

    feat(): new settings controls

--- src/components/RouteViewer/Settings.svelte
+++ src/components/RouteViewer/Settings.svelte
@@ -1,9 +1,12 @@
 <div class="settings">
     <div class="item">
-        number of routes to generate: <input type="number" bind:value={settings.limit} />
+        number of routes to generate: <input type="number" min="1" bind:value={settings.limit} />
     </div>
     <div class="item">
-        max route length: <input type="range" bind:value={settings.maxRouteLength} min={1} max={5} /> {settings.maxRouteLength}
+        max length multiple: <input type="range" bind:value={settings.maxLengthMultiple} min={1} max={5} /> {settings.maxLengthMultiple}
+    </div>
+    <div class="item">
+        insert only if route is shorter: <input type="checkbox" bind:checked={settings.insertOnlyShorter} />
     </div>
 </div>
 <script lang="ts">
@@ -11,7 +14,8 @@
 
     export let settings: GenerateSettings = {
         limit: 10,
-        maxRouteLength: 2
+        maxLengthMultiple: 2,
+        insertOnlyShorter: true
     }
 </script>
 
