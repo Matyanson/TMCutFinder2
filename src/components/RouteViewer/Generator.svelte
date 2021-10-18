bea7d49c1e3fb92fefed946f43df166683f32e9d
Matyanson <matyas.perem@seznam.cz>
2021-10-18 19:15:36 +0200

    feat: manually add route using string

--- src/components/RouteViewer/Generator.svelte
+++ src/components/RouteViewer/Generator.svelte
@@ -5,6 +5,13 @@
     {#if w}
     <ProgressBar bind:progress />
     {/if}
+    add new route:
+    <input type="text" bind:value={newRouteStr} /> <button on:click={addRoute} >add</button>
+    <div class="routes">
+    {#each manualRoutes as r, i}
+        <RouteBtn selected={i == -(selected - 1)} route={r} on:click={() => selectRoute(-(i+1))} />
+    {/each}
+    </div>
     <div class="routes">
     {#each routes as r, i}
         <RouteBtn selected={i == selected} route={r} on:click={() => selectRoute(i)} />
@@ -21,10 +28,13 @@ import type { WorkerMessage } from "src/models/WorkerMessage";
 import type { Route } from "src/models/Route";
 import RouteBtn from "./RouteBtn.svelte";
 import ProgressBar from "../ProgressBar.svelte";
+import { strToRoute } from "src/utils/data";
 
 let w: Worker;
 let settings: GenerateSettings;
 let routes: Route[] = [];
+let manualRoutes: Route[] = [];
+let newRouteStr: string = "";
 let selected: number;
 let progress: number = 0;
 
@@ -36,9 +46,17 @@ $: {
     selected = -1;
 }
 
-const selectRoute = (i) => {
+const selectRoute = (i) => {    //negative is manual
+    console.log(i);
     selected = i;
-    route = routes[i];
+    route = i < 0 ? manualRoutes[-i-1] : routes[i];
+}
+
+const addRoute = () => {
+    const newRoute = strToRoute(newRouteStr);
+    console.log(newRoute);
+    manualRoutes = [...manualRoutes, newRoute];
+    console.log(manualRoutes);
 }
 
 const startWorker = () => {
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -5,3 +5,15 @@ export const routeToStr = (r: Route) => {
     .map(p => `${p.index},${p.start ? 'S': 'E'};`)
     .reduce((total, cur) => total + cur);
 }
+export const strToRoute = (s: string) => {
+    const res = { points: [], dist: 0, cps: []};
+    const pointsStr = s.split(';');
+    for(const p of pointsStr){
+        const vals = p.split(',');
+        res.points.push({
+            index: Number(vals[0]),
+            start: vals[1] == 'S' ? true : false
+        })
+    }
+    return res;
+}
