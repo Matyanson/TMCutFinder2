ee0aeccdfa8874187fa51481e798d1e7186f84bb
Matyanson <matyas.perem@seznam.cz>
2024-05-08 22:09:59 +0200

    feat(search): reverse search v2

--- src/pathFinding/index.ts
+++ src/pathFinding/index.ts
@@ -1,8 +1,9 @@
 import bruteForceSearch from './bruteForceSearch';
 import reverseSearch from './reverseSearch';
+import reverseSearch2 from './reverseSearch2';
 import reverseSearch3 from './reverseSearch3';
 
-const algorithms = [bruteForceSearch, reverseSearch, reverseSearch3];
+const algorithms = [bruteForceSearch, reverseSearch, reverseSearch2, reverseSearch3];
 
 export const algorithmNames = algorithms.map((a) => a.name);
 
